b8b9c9c441a668926de62560116cd3c6bcf0cf4f
gloriapss <88331740+gloriapss@users.noreply.github.com>
2022-06-20 18:46:22 -0500

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,18 @@
+/**
+ * Autentifica los valores que ingresan
+ */
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        if(actualCode === enteredCode.toUpperCase()) 
+            return true
+        else
+            return false
+    }
+
+}
